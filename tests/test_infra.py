8434b1246d51711b338104203b6a61cac7721c36
pedrotenorio05 <pedrohtenorioufrpe@gmail.com>
2026-07-01 15:30:06 -0300

    fix(deps): corrige versao do pacote httpx2 no requirements.txt

--- tests/test_infra.py
+++ tests/test_infra.py
@@ -4,6 +4,7 @@ from fastapi.testclient import TestClient
 from src.banco.conexao import criar_tabelas, engine
 from src.main import app
 
+
 def test_conexao_e_criacao_tabelas():
     try:
         criar_tabelas()
@@ -11,13 +12,14 @@ def test_conexao_e_criacao_tabelas():
     except Exception as e:
         pytest.fail(f"Erro ao conectar ou criar tabelas: {e}")
 
+
 # Adicionamos o patch aqui para "calar" o consumidor durante o teste do FastAPI
 @patch("src.main.iniciar_consumidor")
 def test_api_startup(mock_consumidor):
     # O TestClient aciona automaticamente o "lifespan" do main.py
     with TestClient(app) as client:
         response = client.get("/")
-        
+
         assert response.status_code == 200
         assert response.json()["status"] == "operacional"
         assert response.json()["modulo"] == "M4 - Recorrência Territorial"
